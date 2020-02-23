81b045f9693b853a899ad7ec294cb6cfe35d19ab
df-2020 <61365672+df-2020@users.noreply.github.com>
2020-02-23 15:29:13 -0600

    New functionality and test cases.
    racktest can now insert geojson coordinate data from any geojson object with a rgeo/geo_json as_text method. Added several additional geojson object types to the test case.

--- projecttester.rb
+++ projecttester.rb
@@ -1,14 +1,104 @@
 require_relative 'racktest'
-require 'sequel'
-require 'httparty'
 require "test/unit"
 
 class ProjectTester < Test::Unit::TestCase
 	# Tests gotta start with "test_"
-	def test_blah
-		# You can also use post, put, delete, head, options in the same fashion
-		response = HTTParty.get('http://localhost:9292/?query_string=')
-		puts response.body, response.code, response.message, response.headers.inspect
+	def test_point_posting
+		uri = URI('http://localhost:9292/')
+
+		req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/geo+json')
+
+		req.body = {
+				"type": "LineString",
+				"coordinates": [
+						[0.0, 0.0],
+						[1.0, 1.0]
+				]
+		}.to_json
+		response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+			http.request(req)
+		end
+		puts response.body, response.code, response.message, response.header.inspect
+
+		req.body = {
+				"type": "Feature",
+				"geometry": {
+						"type": "Point",
+						"coordinates": [125.6, 10.1]
+				},
+				"properties": {
+						"name": "Dinagat Islands"
+				}
+		}.to_json
+		response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+			http.request(req)
+		end
+		puts response.body, response.code, response.message, response.header.inspect
+
+		# There is a limitation with rgeo/geo_json where it will throw an error when given an invalid polygon;
+		# as a result, the below case is commented out until a workaround is found.
+		# req.body = {
+		# 		"type": "Polygon",
+		# 		"coordinates": [
+		# 				[
+		# 						[2.0, 6.0],
+		# 						[3.0, 5.0],
+		# 						[4.0, 4.0],
+		# 						[5.0, 3.0],
+		# 						[6.0, 2.0]
+		# 				]
+		# 		]
+		# }.to_json
+		# response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+		# 	http.request(req)
+		# end
+		# puts response.body, response.code, response.message, response.header.inspect
+
+		req.body = {
+				"type": "MultiLineString",
+				"coordinates": [
+						[
+								[7.0, 0.0],
+								[8.0, 1.0]
+						],
+						[
+								[9.0, 2.0],
+								[10.0, 3.0]
+						]
+				]
+		}.to_json
+		response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+			http.request(req)
+		end
+		puts response.body, response.code, response.message, response.header.inspect
+
+
+		req.body = {
+				"type": "GeometryCollection",
+				"geometries": [{
+													 "type": "Point",
+													 "coordinates": [100.0, 0.0]
+											 }, {
+													 "type": "LineString",
+													 "coordinates": [
+															 [101.0, 0.0],
+															 [102.0, 1.0]
+													 ]
+											 }]
+		}.to_json
+		response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+			http.request(req)
+		end
+		puts response.body, response.code, response.message, response.header.inspect
+
+		req = Net::HTTP::Get.new(uri)
+		response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+			http.request(req)
+		end
+		puts response.body, response.code, response.message, response.header.inspect
+
+
+
 	end
 	#Tests to make sure non-post and non-get requests are rejected; tests for invalid data types;
 end
--- racktest.rb
+++ racktest.rb
@@ -2,41 +2,56 @@
 
 require 'rack'
 require 'sequel'
+require 'rgeo/geo_json'
 
 module Rack
   class Racktest
     # connect to the POSTGRES server
     DB = Sequel.connect('postgres://geojson:geojson@localhost:5432/geojson_development')
-    DB["CREATE TABLE IF NOT EXISTS geojson_points (point_id serial PRIMARY KEY, point_geog geography(POINT))"]
+    DB.run("CREATE TABLE IF NOT EXISTS geojson_points (point_id serial PRIMARY KEY, point_geog geography(POINT))")
     QS = "SELECT srid, auth_name, auth_srid, proj4text FROM spatial_ref_sys WHERE cast(SRID as text) LIKE ?"
-    @result = ""
+    @result = nil
     #evn is the environment, containing QUERY_STRING, REQUEST_METHOD, SERVER_NAME, etc. This is how geojson data will be fed to the app
     def call(env)
       # User is giving geojson points; add them to table
-      if env[REQUEST_METHOD] == "POST" && env[CONTENT_TYPE] == "application/geo+json"
-        env[QUERY_STRING].each do |point|
-          #put point into table;
+      if env["REQUEST_METHOD"] == "POST" && env["CONTENT_TYPE"] == "application/geo+json"
+        x_parser = "SELECT ST_X((ST_DumpPoints(ST_GeomFromText(?))).geom)"
+        y_parser = "SELECT ST_Y((ST_DumpPoints(ST_GeomFromText(?))).geom)"
+        input = env['rack.input'].read
+        puts "INPUT: " + input
+        decoded_input = RGeo::GeoJSON.decode(input)
+        puts "CLASS: " + decoded_input.class.to_s
+        # If the decoded input lacks a as_text method, then it is not valid geojson input;
+        # return an error code to the requester
+        unless decoded_input.class.method_defined? :as_text
+          content = "Input is not a valid geojson type."
+          length = content.length.to_s
+          return [500, { CONTENT_TYPE => "text/html", CONTENT_LENGTH => length }, [content]]
         end
-      elsif env[REQUEST_METHOD] == "GET" # Endpoint 2 or 3; if given a point and an int, calc the points with radius; if a polygon, find points within it
+        x_coords = DB[x_parser, RGeo::GeoJSON.decode(input).as_text].all
+        y_coords = DB[y_parser, RGeo::GeoJSON.decode(input).as_text].all
+
+        x_coords.each_with_index do |x_coord, index|
+            puts "X/Y : " + x_coord[:st_x].to_s + "/" + y_coords[index][:st_y].to_s
+            # put point into table; "all" invocation is necessary for data to be commited to DB
+            DB["INSERT INTO geojson_points(point_geog) VALUES (ST_GeomFromText('POINT(? ?)'))", x_coord[:st_x], y_coords[index][:st_y]].all
+        end
+      elsif env["REQUEST_METHOD"] == "GET" # Endpoint 2 or 3; if given a point and an int, calc the points with radius; if a polygon, find points within it
         #Endpoint 2: Use ST_PointInsideCircle?
         #Endpont 3: Use ST_CONTAINS?
         @result = DB[QS, "38%"]
       end
 
-
-      # content = ["<title>My Little Rack App!</title>",
-      #            "<p>", "QUERY STRING: ",env[QUERY_STRING] || "", "</p>","<p>","REQUEST METHOD: ",env[REQUEST_METHOD] || "","</p>",
-      #            "<p>","CONTENT TYPE: ",env[CONTENT_TYPE] || "","</p>","<p>","<a href='#{URI.encode_www_form("CONTENT_TYPE" => "application/geo+json", "REQUEST_METHOD" => "GET", "QUERY_STRING" => qs)}'>","Post it","</a>","</p>",
-      # "<p>","RESULT: ","result","</p>"]
       content = ""
-      @result.each do |row|
-        row.each do |e|
-          content = content + e.join(",")
+      if @result != nil
+        @result.each do |row|
+          row.each do |e|
+            content = content + e.join(",")
+          end
         end
       end
       # Calculate the CONTENT_SIZE by tallying up the size of each element in the content array
 #  inject(0) means put the value 0 into the first element, ie. a
-      #length = content.inject(0) { |a, e| a + e.size }.to_s
       length = content.length.to_s
 #Below is rack's response, consisting of a Status Code, Headers, and a Body.
 #For the GET requests to our app, this is where results will be returned to the user (ie, all points within radius of some point they gave us)
@@ -47,6 +62,7 @@ module Rack
   end
 end
 
+# Start the Rack server
 if $0 == __FILE__
   # :nocov:
   require_relative '../rack'
