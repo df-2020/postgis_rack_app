3a5d794df40721ebf725660d98fa80f3aef4602a
df-2020 <61365672+df-2020@users.noreply.github.com>
2020-02-24 16:03:29 -0600

    Removed defunct test cases

--- geojson_rack_app_tester.rb
+++ geojson_rack_app_tester.rb
@@ -2,6 +2,7 @@ require_relative "geojson_rack_app"
 require "test/unit"
 
 class GeojsonRackAppTester < Test::Unit::TestCase
+  # Test cases for sending points via a POST request
   def test_point_posting
     uri = URI("http://localhost:9292/")
 
@@ -11,30 +12,16 @@ class GeojsonRackAppTester < Test::Unit::TestCase
         "type": "LineString",
         "coordinates": [
             [0.0, 0.0],
-            [1.0, 1.0]
+            [1.0, 1.0],
         ],
-        "srid": 4001
+        "srid": 4001,
     }.to_json
     response = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
-    puts response.body, response.code, response.message, response.header.inspect
 
-    req.body = {
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [125.6, 10.1]
-        },
-        "properties": {
-            "name": "Dinagat Islands"
-        },
-        "srid": 4002
-    }.to_json
-    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-      http.request(req)
-    end
-    puts response.body, response.code, response.message, response.header.inspect
+    assert_equal("200", response.code)
+    assert_equal("OK", response.message)
 
     # There is a limitation with rgeo/geo_json where it will throw an error when given an invalid polygon;
     # as a result, the below case is commented out until a workaround is found.
@@ -53,10 +40,9 @@ class GeojsonRackAppTester < Test::Unit::TestCase
     # response = Net::HTTP.start(uri.hostname, uri.port) do |http|
     #   http.request(req)
     # end
-    # puts response.body, response.code, response.message, response.header.inspect
 
     req.body = {
-				"type": "MultiLineString",
+        "type": "MultiLineString",
         "coordinates": [
             [
                 [7.0, 0.0],
@@ -67,33 +53,55 @@ class GeojsonRackAppTester < Test::Unit::TestCase
                 [10.0, 3.0]
             ]
         ],
-        "srid": 4003
+        "srid": 4003,
     }.to_json
     response = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
-    puts response.body, response.code, response.message, response.header.inspect
+
+    assert_equal("200", response.code)
+    assert_equal("OK", response.message)
 
 
     req.body = {
         "type": "GeometryCollection",
         "geometries": [{
                            "type": "Point",
-                           "coordinates": [102.5, 5.5]
+                           "coordinates": [102.5, 5.5],
                        }, {
                            "type": "LineString",
                            "coordinates": [
                                [101.0, 0.0],
-                               [102.0, 1.0]
+                               [102.0, 1.0],
                            ]
                        }]
     }.to_json
     response = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
-    puts response.body, response.code, response.message, response.header.inspect
+
+    assert_equal("200", response.code)
+    assert_equal("OK", response.message)
+
+    req.body = [{
+                    "type": "Point",
+                    "coordinates": [100.0, 0.0],
+                }, {
+                    "type": "Point",
+                    "coordinates": [105.0, 0.0],
+                }, {
+                    "type": "Point",
+                    "coordinates": [108.0, 0.0],
+                }].to_json
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    assert_equal("200", response.code)
+    assert_equal("OK", response.message)
   end
 
+  # Test cases for determining which points fall within the radius of a given point
   def test_radius_function
     uri = URI("http://localhost:9292/")
     req = Net::HTTP::Get.new(uri)
@@ -101,14 +109,17 @@ class GeojsonRackAppTester < Test::Unit::TestCase
     req.body = {
         "type": "Point",
         "coordinates": [100.0, 0.0],
-        "radius": 3
+        "radius": 3,
     }.to_json
     response = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
-    puts response.body, response.code, response.message, response.header.inspect
+
+    assert_equal("200", response.code)
+    assert_equal("OK", response.message)
   end
 
+  # Test cases for determining which points fall within the area of the given polygon
   def test_polygon_function
     uri = URI("http://localhost:9292/")
     req = Net::HTTP::Get.new(uri)
@@ -117,17 +128,19 @@ class GeojsonRackAppTester < Test::Unit::TestCase
         "type": "Polygon",
         "coordinates": [
             [
-								[102.0, 6.0],
+                [102.0, 6.0],
                 [102.0, 5.0],
                 [103.0, 5.0],
                 [103.0, 6.0],
-                [102.0, 6.0]
+                [102.0, 6.0],
             ]
         ]
     }.to_json
     response = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
-    puts response.body, response.code, response.message, response.header.inspect
+
+    assert_equal("200", response.code)
+    assert_equal("OK", response.message)
   end
 end
