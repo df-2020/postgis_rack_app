8b4d52ed291adef9705f51942aec552eff58dc3b
df-2020 <61365672+df-2020@users.noreply.github.com>
2020-02-23 21:09:00 -0600

    Updated to handle SRIDs more robustly

--- geojson_rack_app.rb
+++ geojson_rack_app.rb
@@ -5,21 +5,27 @@ require "sequel"
 require "rgeo/geo_json"
 
 module Rack
-  class Racktest
+  class GeojsonRackApp
     # connect to the POSTGRES server
     DB = Sequel.connect("postgres://geojson:geojson@localhost:5432/geojson_development")
-    DB.run("CREATE TABLE IF NOT EXISTS geojson_points (point_id serial PRIMARY KEY, point_geom geometry(POINT))")
+    DB.run("CREATE TABLE IF NOT EXISTS geojson_points (point_id serial PRIMARY KEY, point_geom geometry(POINT), srid integer DEFAULT 3857)")
     @result = nil
     # evn is the environment, containing QUERY_STRING, REQUEST_METHOD, SERVER_NAME, etc. This is how geojson data will be fed to the app
     def call(env)
+      # Read the input from the user
+      input = env["rack.input"].read
+      srid = "3857"
+      # If an SRID is supplied, use it in place of our default
+      if JSON.parse(input)["srid"].to_s != ""
+        srid = JSON.parse(input)["srid"].to_s
+      end
       # User is giving geojson points; add them to table
       if env["REQUEST_METHOD"] == "POST" && env["CONTENT_TYPE"] == "application/geo+json"
+        # Statement to retrieve all X coordinates from the input points
         x_parser = "SELECT ST_X((ST_DumpPoints(ST_GeomFromEWKT(?))).geom)"
+        # Statement to retrieve all Y coordinates from the input points
         y_parser = "SELECT ST_Y((ST_DumpPoints(ST_GeomFromEWKT(?))).geom)"
-        input = env["rack.input"].read
-        puts "INPUT: " + input
         decoded_input = RGeo::GeoJSON.decode(input)
-        puts "CLASS: " + decoded_input.class.to_s
         # If the decoded input lacks a as_text method, then it is not valid geojson input;
         # return an error code to the requester
         unless decoded_input.class.method_defined? :as_text
@@ -33,30 +39,31 @@ module Rack
         x_coords.each_with_index do |x_coord, index|
           puts "X/Y : " + x_coord[:st_x].to_s + "/" + y_coords[index][:st_y].to_s
           # put point into table; "all" invocation is necessary for data to be commited to DB; save as 3857 for distance calculations
-          DB["INSERT INTO geojson_points(point_geom) VALUES (ST_GeomFromEWKT('SRID=3857;POINT(? ?)'))", x_coord[:st_x], y_coords[index][:st_y]].all
+          DB["INSERT INTO geojson_points(point_geom, srid) VALUES (ST_GeomFromEWKT('POINT(? ?)'), ?)", x_coord[:st_x], y_coords[index][:st_y], srid].all
         end
       elsif env["REQUEST_METHOD"] == "GET" # Endpoint 2 or 3; if given a point and an int, calc the points with radius; if a polygon, find points within it
-        input = env["rack.input"].read
-        puts "INPUT: " + input
         decoded_input = RGeo::GeoJSON.decode(input)
         puts "CLASS: " + decoded_input.class.to_s
         if decoded_input.class.to_s == "RGeo::Cartesian::PointImpl"
           puts "Decoded input: " + decoded_input.as_text
           puts "Radius: " + JSON.parse(input)["radius"].to_s
           qs = "SELECT gp.point_id, ST_AsText(gp.point_geom) FROM geojson_points gp WHERE ST_DWithin(ST_GeomFromEWKT(?), gp.point_geom, ?) = TRUE"
-          results = DB[qs, "SRID=3857;" + decoded_input.as_text, JSON.parse(input)["radius"]]
+          # Extended Well-Known text (EWKT)
+          ewkt = "SRID=" + srid + ";" + decoded_input.as_text
+          results = DB[qs, ewkt, JSON.parse(input)["radius"]]
           results.each do |r|
             puts "RADIUS RESULT: " + r.to_s
           end
         elsif decoded_input.class.to_s == "RGeo::Cartesian::PolygonImpl"
           qs = "SELECT gp.point_id FROM geojson_points gp WHERE ST_Contains(ST_GeomFromEWKT(?), gp.point_geom) = TRUE"
+          # Extended Well-Known text (EWKT)
+          ewkt = "SRID=" + srid + ";" + decoded_input.as_text
           # Append the SRID for distance calculations
-          results = DB[qs, "SRID=3857;" + decoded_input.as_text].all
+          results = DB[qs, ewkt].all
           results.each do |r|
             puts "POLY RESULT: " + r.to_s
           end
         end
-        # Endpoint 2: Use ST_PointInsideCircle?
       end
 
       content = ""
@@ -82,7 +89,7 @@ if $0 == __FILE__
   # :nocov:
   require_relative "../rack"
   Rack::Server.start(
-      app: Rack::ShowExceptions.new(Rack::Lint.new(Rack::Racktest.new)), Port: 9292
+      app: Rack::ShowExceptions.new(Rack::Lint.new(Rack::GeojsonRackApp.new)), Port: 9292
   )
   # :nocov:
 end
