4c465ac871404efdf9f36d0636c78667d3ac16f4
df-2020 <61365672+df-2020@users.noreply.github.com>
2020-02-24 17:31:45 -0600

    More restructuring, proper return values

--- geojson_rack_app.rb
+++ geojson_rack_app.rb
@@ -20,16 +20,7 @@ module Rack
       # We've been given an array, presumably composed of point objects.
       # Decode them into GeoJSON and insert them
       if JSON.parse(input).class.to_s == "Array"
-        # Initialize SRID
-        @srid = "3857"
-        number_of_inserts = 0
-        JSON.parse(input).each do |x|
-          @decoded_input = RGeo::GeoJSON.decode(x)
-          insert_points
-          number_of_inserts += 1
-        end
-        content = "Inserted " + number_of_inserts.to_s + " points into the database."
-        return [200, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => content.length.to_s}, [content]]
+        return insert_array_of_points(input)
       end
       # If an SRID is supplied, use it. Oherwise, use our default.
       parsed_srid = JSON.parse(input)["srid"].to_s
@@ -44,36 +35,28 @@ module Rack
       # return an error code to the requester
       unless @decoded_input.class.method_defined? :as_text
         content = "Input is not a valid GeoJSON Geometry type."
-        length = content.length.to_s
-        return [500, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => length}, [content]]
+        return [500, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => content.length.to_s}, [content]]
       end
       # User is giving geojson points; add them to table
       if env["REQUEST_METHOD"] == "POST" && env["CONTENT_TYPE"] == "application/geo+json"
         insert_points
-      elsif env["REQUEST_METHOD"] == "GET" # If given a point and an int, calc the points within radius; if a polygon, find points within it
+      elsif env["REQUEST_METHOD"] == "GET"
+        # The input is a Point; find all other points within the given radius
         if @decoded_input.class.to_s == "RGeo::Cartesian::PointImpl"
           radius = JSON.parse(input)["radius"]
           points_within_radius(radius)
+        # The input is a Polygon; find all points that lie within the polygon
         elsif @decoded_input.class.to_s == "RGeo::Cartesian::PolygonImpl"
           points_within_polygon
+        # The Input is invalid
         else
           content = "Invalid GeoJSON object; Please GET with only a GeoJSON Point or Polygon."
-          return [500, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => content.length.to_s}, [content]]
+          [500, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => content.length.to_s}, [content]]
         end
+      else
+        content = "Invalid request; Please issue a GET with either a GeoJSON Polygon, or a GeoJSON Point with an integer radius, or issue a POST with an array of GeoJSON Points or a GeometryCollection."
+        [500, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => content.length.to_s}, [content]]
       end
-
-      content = ""
-      @result&.each do |row|
-        row.each do |e|
-          content += e.join(",")
-        end
-      end
-      # Calculate the CONTENT_SIZE by tallying up the size of each element in the content array
-      #  inject(0) means put the value 0 into the first element, ie. a
-      length = content.length.to_s
-      # Below is rack's response, consisting of a Status Code, Headers, and a Body.
-      # For the GET requests to our app, this is where results will be returned to the user (ie, all points within radius of some point they gave us)
-      [200, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => length}, [content]]
     end
 
     private
@@ -91,6 +74,8 @@ module Rack
         # put point into table; "all" invocation is necessary for data to be commited to DB; save SRID into point's geometry as well as in table column
         DB["INSERT INTO geojson_points(point_geom, srid) VALUES (ST_GeomFromEWKT('SRID=?;POINT(? ?)'), ?)", Integer(@srid), x_coord[:st_x], y_coords[index][:st_y], @srid].all
       end
+      content = "Inserted " + x_coords.length.to_s + " points into the database."
+      [200, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => content.length.to_s}, [content]]
     end
 
     # Method to calculate which points in our DB fall within the given radius value from a given point
@@ -98,10 +83,9 @@ module Rack
       qs = "SELECT gp.point_id, ST_AsText(gp.point_geom) FROM geojson_points gp WHERE ST_DWithin(ST_GeomFromEWKT(?), gp.point_geom, ?) = TRUE"
       # Extended Well-Known text (EWKT)
       ewkt = "SRID=" + @srid + ";" + @decoded_input.as_text
-      results = DB[qs, ewkt, radius]
-      results.each do |r|
-        puts "RADIUS RESULT: " + r.to_s
-      end
+      results = DB[qs, ewkt, radius].all
+      content = results.to_json #convert to geojson
+      [200, {CONTENT_TYPE => "application/geo+json", CONTENT_LENGTH => content.length.to_s}, [content]]
     end
 
     # Method to determine which points in our DB fall within a given Polygon from a GET request
@@ -111,9 +95,21 @@ module Rack
       ewkt = "SRID=" + @srid + ";" + @decoded_input.as_text
       # Append the SRID for distance calculations
       results = DB[qs, ewkt].all
-      results.each do |r|
-        puts "POLY RESULT: " + r.to_s
+      content = results.to_json #convert to geojson?
+      [200, {CONTENT_TYPE => "application/geo+json", CONTENT_LENGTH => content.length.to_s}, [content]]
+    end
+
+    def insert_array_of_points(array_of_points)
+      # Initialize SRID
+      @srid = "3857"
+      number_of_inserts = 0
+      JSON.parse(array_of_points).each do |point|
+        @decoded_input = RGeo::GeoJSON.decode(point)
+        insert_points
+        number_of_inserts += 1
       end
+      content = "Inserted " + number_of_inserts.to_s + " points into the database."
+      [200, {CONTENT_TYPE => "text/html", CONTENT_LENGTH => content.length.to_s}, [content]]
     end
   end
 end
